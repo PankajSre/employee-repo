101424ec339e46c05b7a9ac3348095f84b18b26a
PankajSre <pankaj.saini85@gmail.com>
2021-09-20 15:05:10 +0530

    Second Commit

---
+++ Course.java
@@ -0,0 +1,5 @@
+public class Course {
+  public static void main(String[] args) {
+    System.out.println("Hello Git from Course class!");
+  }
+}
--- Customer.java
+++ Customer.java
@@ -1,5 +1,6 @@
 public class Customer {
   public static void main(String[] args) {
     System.out.println("Hello Git from Customer!");
+	System.out.println("Hello Git from Customer Again!");
   }
 }
