695296919300ce4a0d51c8b8912e6bef031d7d94
PankajSre <pankaj.saini85@gmail.com>
2021-09-27 14:25:58 +0530

    Fifth Commit

--- Course.java
+++ Course.java
@@ -2,5 +2,6 @@ public class Course {
   public static void main(String[] args) {
     System.out.println("Hello jenkins from Github");
 	System.out.println("Hello Git from Course class");
+	System.out.println("Auto trigger form the Github!");
   }
 }
--- Customer.java
+++ Customer.java
@@ -2,5 +2,6 @@ public class Customer {
   public static void main(String[] args) {
     System.out.println("Hello Jenkins from Customer!");
 	System.out.println("Hello Git from Customer !");
+	System.out.println("Auto trigger from the Github !");
   }
 }
